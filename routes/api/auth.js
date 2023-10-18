7672a4a380908f58bce21a2c3e0aef9dc57eaf71
MarinaSigida <sigida.web@gmail.com>
2023-10-18 18:02:41 +0200

    add: avatar upload

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const { validateBody, authenticate} = require("../../middlewares");
+const express = require("express");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
-const auth = require('../../controllers/auth');
+const auth = require("../../controllers/auth");
 const router = express.Router();
-
-
 
 router.post("/register", validateBody(schemas.registerSchema), auth.register);
 
@@ -15,10 +13,17 @@ router.get("/current", authenticate, auth.getCurrent);
 router.post("/logout", authenticate, auth.logout);
 
 router.patch(
-    "/users",
-    authenticate,
-    validateBody(schemas.updateSubscriptionSchema),
-    auth.updateUserSubscription
-  );
+  "/users",
+  authenticate,
+  validateBody(schemas.updateSubscriptionSchema),
+  auth.updateUserSubscription
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  auth.updateAvatar
+);
 
 module.exports = router;
